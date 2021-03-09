544d959bb88e4e876fa9fb08c86ec1bd58000e83
Vladislav Zlatanov <vladislav_zlatanov@epam.com>
2021-03-09 14:25:40 +0200

    add Spring Boot for calculator module

--- calculator/src/main/java/com/market/banica/calculator/CalculatorApplication.java
+++ calculator/src/main/java/com/market/banica/calculator/CalculatorApplication.java
@@ -1,9 +1,13 @@
 package com.market.banica.calculator;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class CalculatorApplication {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello calculator");
+        SpringApplication.run(CalculatorApplication.class, args);
     }
 }
