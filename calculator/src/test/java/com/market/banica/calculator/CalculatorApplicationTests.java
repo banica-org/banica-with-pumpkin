79a835624e98590bbdd9b8b1f558ecaac8283a31
Detelin_Radev <Detelin_Radev@epam.com>
2021-03-09 17:39:38 +0200

    Create base package structure, add maven dependencies

---
+++ calculator/src/test/java/com/market/banica/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,12 @@
+package com.market.banica.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CalculatorApplicationTests {
+
+    @Test
+    void contextLoads(){
+    }
+}
