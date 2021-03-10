46912c1bd9879796700a203d6776e0d9bb9d0c1f
Valentin_Tomov <Valentin_Tomov@epam.com>
2021-03-10 17:21:02 +0200

    Orderbook service - data model/proto definition for orderbook ticks, added dependencies for spring and grpc

--- order-book/src/main/java/com/market/banica/order/book/OrderBookApplication.java
+++ order-book/src/main/java/com/market/banica/order/book/OrderBookApplication.java
@@ -1,5 +1,8 @@
 package com.market.banica.order.book;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
 public class OrderBookApplication {
 
     public static void main(String[] args) {
