8003e52f3b8147284463ad50a8b54c3c09bfeadc
Valentin_Tomov <Valentin_Tomov@epam.com>
2021-03-14 15:26:48 +0200

    Add Logger , implement stop method

--- order-book/src/main/java/com/market/banica/order/book/MarketDataClient.java
+++ order-book/src/main/java/com/market/banica/order/book/MarketDataClient.java
@@ -6,19 +6,23 @@ import epam.market.banica.order.book.grpc.TickResponse;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
-import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.util.concurrent.TimeUnit;
 
 @Service
-@RequiredArgsConstructor
+
 public class MarketDataClient {
 
     private final ItemMarket itemMarket;
     private final ManagedChannel managedChannel;
+    private static final Logger LOGGER = LoggerFactory.getLogger(MarketDataClient.class);
 
     @Autowired
     MarketDataClient(ItemMarket itemMarket,
@@ -30,43 +34,44 @@ public class MarketDataClient {
         this.itemMarket = itemMarket;
     }
 
+    @PostConstruct
     private void start() {
         for (String product : itemMarket.getAllProductNames()) {
             final MarketDataServiceGrpc.MarketDataServiceStub asynchronousStub = MarketDataServiceGrpc.newStub(managedChannel);
             final MarketDataRequest request = MarketDataRequest.newBuilder()
                     .setItemName(product)
                     .build();
+            LOGGER.debug("Start gathering product data.");
             asynchronousStub.getMarketData(request, new StreamObserver<TickResponse>() {
 
                 @Override
                 public void onNext(TickResponse response) {
-
                     Item item = new Item();
                     item.setPrice(response.getPrice());
                     item.setQuantity((int) response.getQuantity());
                     item.getItemIDs().add(new Item.ItemID("1", response.getOrigin().toString()));
+                    LOGGER.debug("Products data updated!");
 
                     itemMarket.getAllItemsByName(response.getItemName()).add(item);
                 }
 
                 @Override
                 public void onError(final Throwable throwable) {
-                    System.out.println(("Unable to request"));
+                    LOGGER.debug("Unable to request");
                 }
 
                 @Override
                 public void onCompleted() {
-                    System.out.println(("Market data gathered"));
+                    LOGGER.debug("Market data gathered");
                 }
             });
         }
     }
 //        exponential backoff! check
 
-
     @PreDestroy
-    private void stop() {
-        managedChannel.shutdownNow();
+    private void stop() throws InterruptedException {
+        managedChannel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+        LOGGER.debug("Server is terminated!");
     }
 }
-
--- order-book/src/main/java/com/market/banica/order/book/OrderBookApplication.java
+++ order-book/src/main/java/com/market/banica/order/book/OrderBookApplication.java
@@ -3,7 +3,6 @@ package com.market.banica.order.book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class OrderBookApplication {
 
