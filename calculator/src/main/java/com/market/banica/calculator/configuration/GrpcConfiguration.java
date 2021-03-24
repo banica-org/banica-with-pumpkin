b61f0d2a3c41135d167d3d8202ab04500f54d59e
Radoslav Rusev <radoslav_rusev@epam.com>
2021-03-24 14:13:15 +0200

    Add feature RBSFXIRJTP-209
    - Create generic GrpcChannel in common module.
    - Add GrpcConfiguration class, which initialize AuroraServiceBlockingStub.
    - Refactor AuroraClientSideService class.

---
+++ calculator/src/main/java/com/market/banica/calculator/configuration/GrpcConfiguration.java
@@ -0,0 +1,16 @@
+package com.market.banica.calculator.configuration;
+
+import com.aurora.AuroraServiceGrpc;
+import com.market.banica.common.channel.GrpcChannel;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GrpcConfiguration {
+
+    @Bean
+    public AuroraServiceGrpc.AuroraServiceBlockingStub getAuroraBlockingStub(@Value("${aurora.server.host}") String host, @Value("${aurora.server.port}") final int port) {
+        return AuroraServiceGrpc.newBlockingStub(new GrpcChannel(host, port).getManagedChannel());
+    }
+}
--- calculator/src/main/java/com/market/banica/calculator/service/grpc/AuroraClientSideService.java
+++ calculator/src/main/java/com/market/banica/calculator/service/grpc/AuroraClientSideService.java
@@ -4,27 +4,12 @@ package com.market.banica.calculator.service.grpc;
 import com.aurora.Aurora;
 import com.aurora.AuroraServiceGrpc;
 import com.market.banica.calculator.exception.exceptions.BadResponseException;
-import com.market.banica.common.ChannelRPCConfig;
 import com.orderbook.ItemOrderBookResponse;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PreDestroy;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-
-/**
- * Date: 3/11/2021 Time: 12:35 PM
- * <p>
- *
- * @author Vladislav_Zlatanov
- */
 
 @Service
 public class AuroraClientSideService {
@@ -33,34 +18,17 @@ public class AuroraClientSideService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AuroraClientSideService.class);
 
-    /**
-     * Channel for gRPC connection with aurora
-     */
-    private final ManagedChannel managedChannel;
+    private final AuroraServiceGrpc.AuroraServiceBlockingStub blockingStub;
 
-    /**
-     * @param host host where aurora is started
-     * @param port port on the server.
-     */
     @Autowired
-    AuroraClientSideService(@Value("${aurora.server.host}") final String host,
-                            @Value("${aurora.server.port}") final int port) {
-
-        LOGGER.info("Setting up connection with Aurora.");
-
-        managedChannel = ManagedChannelBuilder.forAddress(host, port)
-                .usePlaintext()
-                .defaultServiceConfig(ChannelRPCConfig.getInstance().getServiceConfig())
-                .enableRetry()
-                .build();
-
+    public AuroraClientSideService(AuroraServiceGrpc.AuroraServiceBlockingStub blockingStub) {
+        this.blockingStub = blockingStub;
     }
 
     public void announceInterests(String productName) {
 
         LOGGER.debug("Inside announceInterests method");
         LOGGER.debug("Building blocking stub");
-        AuroraServiceGrpc.AuroraServiceBlockingStub blockingStub = getBlockingStub();
 
         LOGGER.debug("Building request with parameters {}", productName);
         Aurora.AuroraRequest request = Aurora.AuroraRequest.newBuilder()
@@ -79,7 +47,6 @@ public class AuroraClientSideService {
 
         LOGGER.debug("Inside getIngredient method");
         LOGGER.debug("Building blocking stub");
-        AuroraServiceGrpc.AuroraServiceBlockingStub blockingStub = getBlockingStub();
 
         LOGGER.debug("Building request with parameters {}", message);
         Aurora.AuroraRequest request = Aurora.AuroraRequest.newBuilder()
@@ -98,18 +65,4 @@ public class AuroraClientSideService {
         return auroraResponse.getItemOrderBookResponse();
 
     }
-
-    private AuroraServiceGrpc.AuroraServiceBlockingStub getBlockingStub() {
-        return AuroraServiceGrpc.newBlockingStub(managedChannel);
-    }
-
-    /**
-     * Shutting down channel before spring removes bean from application context
-     */
-    @PreDestroy
-    private void shutdown() throws InterruptedException {
-        LOGGER.info("Shutting down connection with Aurora.");
-        managedChannel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
-    }
-
 }
