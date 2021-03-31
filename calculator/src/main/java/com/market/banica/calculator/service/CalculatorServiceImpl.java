e494e9356bfc7c8152f9f5086ac4fb3bd159d980
Radoslav Rusev <radoslav_rusev@epam.com>
2021-03-31 09:09:32 +0300

    Create getProductWithAppropriateIngredients which returns product name and its ingredients.

--- calculator/src/main/java/com/market/banica/calculator/service/CalculatorServiceImpl.java
+++ calculator/src/main/java/com/market/banica/calculator/service/CalculatorServiceImpl.java
@@ -6,11 +6,13 @@ import com.market.banica.calculator.service.contract.CalculatorService;
 import com.market.banica.calculator.service.contract.ProductService;
 import com.market.banica.calculator.service.grpc.AuroraClientSideService;
 import com.orderbook.ItemOrderBookResponse;
+import com.orderbook.OrderBookLayer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @Service
@@ -26,14 +28,24 @@ public class CalculatorServiceImpl implements CalculatorService {
 
         List<Product> products = productService.getProductAsListProduct(itemName);
 
-        List<ItemOrderBookResponse> resultList = new ArrayList<>();
-
-        for (Product product : products) {
-            resultList.add(auroraService.getIngredient(product.getProductName(),clientId));
+        while (quantity > 0){
+            Map<String, List<OrderBookLayer>> productAvailability = getProductWithAppropriateIngredients(products, clientId);
         }
 
-        //TODO
 
-        return null;
+        return new RecipeDTO();
+    }
+
+    private Map<String, List<OrderBookLayer>> getProductWithAppropriateIngredients(List<Product> products, String clientId) {
+        Map<String, List<OrderBookLayer>> productAvailability = new HashMap<>();
+
+        for (Product product : products) {
+            ItemOrderBookResponse itemOrderBookResponse = auroraService.getIngredient(product.getProductName(), clientId);
+
+            String productName = itemOrderBookResponse.getItemName();
+            List<OrderBookLayer> productLayers = itemOrderBookResponse.getOrderbookLayersList();
+            productAvailability.put(productName, productLayers);
+        }
+        return productAvailability;
     }
 }
