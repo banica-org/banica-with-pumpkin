ea013fa20b3ab37c7f404406f96f31a034cdf8b7
Angel Pashunov <Angel_Pashunov@epam.com>
2021-04-01 18:05:08 +0300

    Add loggers and refactor code

--- calculator/src/main/java/com/market/banica/calculator/Runner.java
+++ calculator/src/main/java/com/market/banica/calculator/Runner.java
@@ -9,7 +9,7 @@ import io.grpc.ManagedChannelBuilder;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class Runner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
