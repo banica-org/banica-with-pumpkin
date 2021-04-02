398b4c7179625318d41e87604d7d94a5796a0b87
Detelin_Radev <Detelin_Radev@epam.com>
2021-04-02 13:22:29 +0300

    Fix bug with printing deleted result, add connection with aurora, add quantity resolver for retrieving products from aurora

--- calculator/src/main/java/com/market/banica/calculator/service/CalculatorServiceImpl.java
+++ calculator/src/main/java/com/market/banica/calculator/service/CalculatorServiceImpl.java
@@ -18,6 +18,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -30,12 +31,18 @@ public class CalculatorServiceImpl implements CalculatorService {
 
     private final AuroraClientSideService auroraService;
     private final ProductService productService;
-    private final TestData testData;
+//    private final TestData testData;
 
     @Override
     public List<ProductDto> getRecipe(String clientId, String itemName, int quantity) {
 
         Set<Product> products = productService.getProductAsListProduct(itemName);
+
+        Gson gson = new Gson();
+        String jsonString = gson.toJson(products);
+        Type type = new TypeToken<HashSet<Product>>() {
+        }.getType();
+        Set<Product> productsVerifyParentSet = gson.fromJson(jsonString, type);
 
         List<ProductDto> result = new ArrayList<>();
 
@@ -44,7 +51,18 @@ public class CalculatorServiceImpl implements CalculatorService {
         Map<String, List<ProductSpecification>> productSpecificationMap = new HashMap<>();
 
         for (Product product : products) {
-            getProductsDataFromOrderBook(clientId, product, quantity, productDtoMap,
+
+            long tempQuantity = quantity;
+            for (Product setProduct : productsVerifyParentSet) {
+
+                if (setProduct.getIngredients().containsKey(product.getProductName())) {
+
+                    tempQuantity = setProduct.getIngredients().get(product.getProductName());
+                    setProduct.getIngredients().remove(product.getProductName());
+                    break;
+                }
+            }
+            getProductsDataFromOrderBook(clientId, product, tempQuantity, productDtoMap,
                     productSpecificationMap);
         }
 
@@ -58,10 +76,8 @@ public class CalculatorServiceImpl implements CalculatorService {
             }
         }
 
-        Gson gson = new Gson();
-
-        String jsonString = gson.toJson(compositeProductsDtoMap);
-        Type type = new TypeToken<HashMap<String, ProductDto>>() {
+        jsonString = gson.toJson(compositeProductsDtoMap);
+        type = new TypeToken<HashMap<String, ProductDto>>() {
         }.getType();
         Map<String, ProductDto> compositeProductsDtoVerifyParentMap = gson.fromJson(jsonString, type);
 
@@ -98,7 +114,7 @@ public class CalculatorServiceImpl implements CalculatorService {
             }
 
             productPrice = productPrice.add(checkPriceForProduct(tempProduct,
-                    orderedProductQuantity,productSpecificationMap));
+                    orderedProductQuantity, productSpecificationMap));
 
 
             for (String productDtoName : tempProduct.getIngredients().keySet()) {
@@ -110,16 +126,19 @@ public class CalculatorServiceImpl implements CalculatorService {
                 if (!compositeProductsDtoMap.containsKey(productDtoName)) {
 
                     tempIngredientPrice = tempIngredientPrice.add(checkPriceForProduct(currentProductDto,
-                            ingredientRecipeQuantity,productSpecificationMap));
+                            ingredientRecipeQuantity, productSpecificationMap));
 
                 } else {
 
-                    tempIngredientPrice = currentProductDto.getTotalPrice()
-                            .multiply(BigDecimal.valueOf(ingredientRecipeQuantity));
+                    tempIngredientPrice = currentProductDto.getTotalPrice();
+
                 }
                 ingredientsPrice = ingredientsPrice.add(tempIngredientPrice);
 
             }
+
+
+            ingredientsPrice = ingredientsPrice.multiply(BigDecimal.valueOf(orderedProductQuantity));
 
             if (productPrice.compareTo(ingredientsPrice) > 0) {
 
@@ -149,14 +168,40 @@ public class CalculatorServiceImpl implements CalculatorService {
             } else {
 
                 tempProduct.setTotalPrice(productPrice);
+
+                for (String ingredientName : tempProduct.getIngredients().keySet()) {
+                    ProductDto productDto = productDtoMap.get(ingredientName);
+                    if (!productDto.getIngredients().isEmpty()) {
+                        for (String tempIngredientName : productDto.getIngredients().keySet()) {
+                            ProductDto tempProductDto = productDtoMap.get(tempIngredientName);
+                            long ingredientQuantity = productDto.getIngredients().get(tempIngredientName);
+                            if (result.contains(tempProductDto)) {
+                                ProductSpecification productSpecification = tempProductDto.getProductSpecifications().stream()
+                                        .filter(k -> k.getQuantity() == ingredientQuantity)
+                                        .findFirst()
+                                        .orElseThrow(IllegalArgumentException::new);
+                                tempProductDto.getProductSpecifications().remove(productSpecification);
+                                if (tempProductDto.getProductSpecifications().isEmpty()) {
+                                    result.remove(tempProductDto);
+                                }
+                            }
+                        }
+                    }
+                    if (result.contains(productDto)){
+                        if(productDto.getProductSpecifications().isEmpty() || productDto.getProductSpecifications().size() ==1){
+                            result.remove(productDto);
+                        }
+                    }
+                }
+
                 tempProduct.getIngredients().clear();
-                writePriceToProduct(tempProduct,orderedProductQuantity,productSpecificationMap);
+                writePriceToProduct(tempProduct, orderedProductQuantity, productSpecificationMap);
             }
 
             result.add(tempProduct);
         }
 
-        if(!compositeProductsDtoMap.get(itemName).getProductSpecifications().isEmpty()){
+        if (!compositeProductsDtoMap.get(itemName).getProductSpecifications().isEmpty()) {
 
             return Collections.singletonList(compositeProductsDtoMap.get(itemName));
         }
@@ -165,7 +210,7 @@ public class CalculatorServiceImpl implements CalculatorService {
     }
 
     private BigDecimal writePriceToProduct(final ProductDto productDto, final long orderedProductQuantity,
-                                     final Map<String, List<ProductSpecification>> productSpecificationMap) {
+                                           final Map<String, List<ProductSpecification>> productSpecificationMap) {
 
         BigDecimal result = BigDecimal.ZERO;
         long productQuantity = orderedProductQuantity;
@@ -232,10 +277,10 @@ public class CalculatorServiceImpl implements CalculatorService {
         return result;
     }
 
-    private void getProductsDataFromOrderBook(String clientId, Product product, int quantity, Map<String,
+    private void getProductsDataFromOrderBook(String clientId, Product product, long quantity, Map<String,
             ProductDto> productDtoMap, Map<String, List<ProductSpecification>> productSpecificationMap) {
 
-        ItemOrderBookResponse orderBookResponse = testData.getTestData().get(product.getProductName());
+        ItemOrderBookResponse orderBookResponse = auroraService.getIngredient(product.getProductName(),clientId,quantity);
 
         String productName = orderBookResponse.getItemName();
 
--- calculator/src/main/java/com/market/banica/calculator/service/TestData.java
+++ calculator/src/main/java/com/market/banica/calculator/service/TestData.java
@@ -13,16 +13,16 @@ public class TestData {
 
     public Map<String, ItemOrderBookResponse> getTestData() {
         Map<String, ItemOrderBookResponse> data = new LinkedHashMap<>();
-        addProductToDatabase(data, "banica", 156000.0, 1);//2+0.9+124 +12750 =12876,9
-        addProductToDatabase(data, "pumpkin", 0.003, 300);
-        addProductToDatabase(data, "milk", 1.0, 2);
-        addProductToDatabase(data, "crusts", 0.0001, 200);//0,5+0.12=0,62
+        addProductToDatabase(data, "banica", 1000000, 2);//pumpkin:450,milk:400,crusts:700,sauce:997725=998775*2=1997550
+        addProductToDatabase(data, "pumpkin", 1.5, 300);
+        addProductToDatabase(data, "milk", 2, 200);
+        addProductToDatabase(data, "crusts", 3.9, 200);//water:0,5,eggs:3=3.5 * 200 = 700
         addProductToDatabase(data, "water", 0.01, 300);
-        addProductToDatabase(data, "eggs", 0.01, 12);
-        addProductToDatabase(data, "sauce", 4.0, 150);//1,5 +1 + 82,5 =85
-        addProductToDatabase(data, "sugar", 0.02, 50);
-        addProductToDatabase(data, "ketchup", 0.03, 50);//0.65+1 = 1,65
-        addProductToDatabase(data, "tomatoes", 0.01, 65);
+        addProductToDatabase(data, "eggs", 0.25, 12);
+        addProductToDatabase(data, "sauce", 7000, 150);//ketchup:6550,water:1.5,sugar:100 =6651,5 * 150=997725
+        addProductToDatabase(data, "sugar", 2, 50);
+        addProductToDatabase(data, "ketchup", 130, 50);//tomatoes:130,water:1 = 131 * 50 = 6550
+        addProductToDatabase(data, "tomatoes", 2, 65);
         return data;
     }
 
