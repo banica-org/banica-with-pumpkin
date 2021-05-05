f0a2bde167829a9fe5f8ef8074395fa54087e056
Angel Pashunov <Angel_Pashunov@epam.com>
2021-05-05 17:31:38 +0300

    Fix import in MarketStateImplTest class

--- market-generator/src/test/java/com/market/banica/generator/service/MarketStateImplTest.java
+++ market-generator/src/test/java/com/market/banica/generator/service/MarketStateImplTest.java
@@ -19,6 +19,7 @@ import org.springframework.test.util.ReflectionTestUtils;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
