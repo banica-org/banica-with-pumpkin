4e4f4eb65977862ca7d6529e48a36def6ce09fd3
Nikola Nguen <nikola_nguen@epam.com>
2021-05-17 14:42:31 +0300

    Refactored the MarketTickObserver so that it can receive the backpressure signals from the BackPressureManager and also implemented new beforeStart method and added functionality in the onNext method that blocks the grpc-transport threads, this way we are blocking the incoming messages from the server and making the server buffer the messages util the backpressure is stopped.

--- aurora/src/main/java/com/market/banica/aurora/observer/MarketTickObserver.java
+++ aurora/src/main/java/com/market/banica/aurora/observer/MarketTickObserver.java
@@ -2,43 +2,90 @@ package com.market.banica.aurora.observer;
 
 import com.aurora.Aurora;
 import com.google.protobuf.Any;
+import com.market.MarketDataRequest;
 import com.market.TickResponse;
+import com.market.banica.aurora.backpressure.BackPressureManager;
 import com.orderbook.ReconnectionResponse;
 import io.grpc.Status;
+import io.grpc.stub.ClientCallStreamObserver;
+import io.grpc.stub.ClientResponseObserver;
 import io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class MarketTickObserver implements StreamObserver<TickResponse> {
+public class MarketTickObserver implements ClientResponseObserver<MarketDataRequest, TickResponse> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MarketTickObserver.class);
 
-    AtomicInteger openStreams;
+    private final AtomicInteger openStreams;
 
     private final String client;
     private final String destinationOfMessages;
     private final String item;
+    private final String orderBookIdentifier;
 
     private final StreamObserver<Aurora.AuroraResponse> forwardResponse;
 
-    public MarketTickObserver(String client, StreamObserver<Aurora.AuroraResponse> forwardResponse, AtomicInteger openStreams,
-                              String destinationOfMessages, String item) {
+    private final MarketDataRequest marketDataRequest;
+
+    private final BackPressureManager backPressureManager;
+
+    private final AtomicBoolean backPressureActivated = new AtomicBoolean(false);
+
+    private ClientCallStreamObserver<Aurora.AuroraRequest> requestStream;
+
+    private CountDownLatch countDownLatch = new CountDownLatch(1);
+
+    public MarketTickObserver(String client, StreamObserver<Aurora.AuroraResponse> forwardResponse, AtomicInteger openStreams, String destinationOfMessages,
+                              String item, MarketDataRequest marketDataRequest, BackPressureManager backPressureManager, String orderBookIdentifier) {
         this.client = client;
         this.forwardResponse = forwardResponse;
         this.openStreams = openStreams;
         this.destinationOfMessages = destinationOfMessages;
         this.item = item;
+        this.marketDataRequest = marketDataRequest;
+        this.backPressureManager = backPressureManager;
+        this.orderBookIdentifier = orderBookIdentifier;
+    }
+
+    @Override
+    public void beforeStart(ClientCallStreamObserver requestStream) {
+        LOGGER.debug("Initializing before start");
+        this.requestStream = requestStream;
+        backPressureManager.addMarketTickObserver(this, orderBookIdentifier);
+
+        requestStream.disableAutoRequestWithInitial(1);
+
+        requestStream.setOnReadyHandler(() -> {
+            if (requestStream.isReady()) {
+                requestStream.onNext(marketDataRequest);
+            }
+        });
     }
 
     @Override
     public void onNext(TickResponse tickResponse) {
         LOGGER.debug("Forwarding response to client {}", client);
+
         Aurora.AuroraResponse response = this.wrapResponse(tickResponse);
 
         synchronized (forwardResponse) {
             forwardResponse.onNext(response);
+            if (backPressureActivated.get()) {
+                try {
+                    LOGGER.info("Backpressure activated by orderbook with gRPC port: {}, for item --> {}", orderBookIdentifier, item);
+                    countDownLatch.await();
+                    countDownLatch = new CountDownLatch(1);
+                    LOGGER.info("Backpressure deactivated by orderbook with gRPC port: {}, for item --> {}", orderBookIdentifier, item);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            requestStream.request(backPressureManager.getNumberOfMessagesToBeRequested());
         }
     }
 
@@ -59,13 +106,20 @@ public class MarketTickObserver implements StreamObserver<TickResponse> {
         }
     }
 
-
     @Override
     public void onCompleted() {
         LOGGER.info("Completing stream request for client {}", client);
         if (openStreams.decrementAndGet() == 0) {
             forwardResponse.onCompleted();
         }
+    }
+
+    public CountDownLatch getCountDownLatch() {
+        return countDownLatch;
+    }
+
+    public void setBackPressureForTick(boolean isOn) {
+        this.backPressureActivated.set(isOn);
     }
 
     private ReconnectionResponse buildReconnect() {
@@ -88,5 +142,4 @@ public class MarketTickObserver implements StreamObserver<TickResponse> {
                 .setMessage(Any.pack(reconnectionResponse))
                 .build();
     }
-
 }
