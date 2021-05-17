87007e388e49bafc0fe97d3b2c1b2d8448c98bb9
Nikola Nguen <nikola_nguen@epam.com>
2021-05-17 14:45:02 +0300

    Added the BackPressureManager to be able to receive the backpressure signals from the orderbook. And depending on the signal the manager is either activating the backpressure or deactivating it.

---
+++ aurora/src/main/java/com/market/banica/aurora/backpressure/BackPressureManager.java
@@ -0,0 +1,59 @@
+package com.market.banica.aurora.backpressure;
+
+import com.aurora.Aurora;
+import com.google.protobuf.Any;
+import com.market.MarketDataRequest;
+import com.market.banica.aurora.observer.MarketTickObserver;
+import io.grpc.stub.StreamObserver;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+@NoArgsConstructor
+@Setter
+@Getter
+public class BackPressureManager {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BackPressureManager.class);
+
+    private int numberOfMessagesToBeRequested = 1;
+
+    private final Map<String, Set<MarketTickObserver>> marketTickObservers = new ConcurrentHashMap<>();
+
+    public void activateBackPressure(String orderBookIdentifier, StreamObserver<Aurora.AuroraResponse> responseObserver) {
+        LOGGER.debug("Activating backpressure for orderbook with gRPC port --> {}", orderBookIdentifier);
+        marketTickObservers.get(orderBookIdentifier).forEach(tick -> tick.setBackPressureForTick(true));
+        notifyBackpressureObserver(orderBookIdentifier + "/on", responseObserver);
+    }
+
+    public void deActivateBackPressure(String orderBookIdentifier, StreamObserver<Aurora.AuroraResponse> responseObserver) {
+        LOGGER.debug("Deactivating backpressure for orderbook with gRPC port --> {}", orderBookIdentifier);
+        for (MarketTickObserver marketTickObserver : marketTickObservers.get(orderBookIdentifier)) {
+            marketTickObserver.setBackPressureForTick(false);
+            marketTickObserver.getCountDownLatch().countDown();
+        }
+        notifyBackpressureObserver(orderBookIdentifier + "/off", responseObserver);
+    }
+
+    public void addMarketTickObserver(MarketTickObserver marketTickObserver, String orderBookIdentifier) {
+        marketTickObservers.putIfAbsent(orderBookIdentifier, ConcurrentHashMap.newKeySet());
+        marketTickObservers.get(orderBookIdentifier).add(marketTickObserver);
+    }
+
+    private void notifyBackpressureObserver(String orderBookIdentifier, StreamObserver<Aurora.AuroraResponse> responseObserver) {
+        LOGGER.debug("Notifying backpressure observer --> {}", responseObserver);
+
+        MarketDataRequest marketDataRequest = MarketDataRequest.newBuilder()
+                .setClientId(orderBookIdentifier)
+                .build();
+
+        responseObserver.onNext(Aurora.AuroraResponse.newBuilder().setMessage(Any.pack(marketDataRequest)).build());
+    }
+}
